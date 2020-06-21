32940f28ba73725b5d6bccbcf07f3da5bab2fc66
jasonravagli <jasonravagli@gmail.com>
2020-06-21 20:14:07 +0200

    First commit
    Parent POM and db-module created

---
+++ parent/db-module/src/main/java/it/jasonravagli/manager/gym/db/App.java
@@ -0,0 +1,13 @@
+package it.jasonravagli.manager.gym.db;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ parent/db-module/src/test/java/it/jasonravagli/manager/gym/db/AppTest.java
@@ -0,0 +1,20 @@
+package it.jasonravagli.manager.gym.db;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
