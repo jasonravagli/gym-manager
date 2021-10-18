4f9bb91fbe77eedbc1466679394be7536a9595e0
jasonravagli <jasonravagli@gmail.com>
2021-10-18 15:58:57 +0200

    Added PIT support for the Maven build
    PIT can be enable specifying the plugin "pit". Activated by default in
    the main GitHub workflow
    
    https://github.com/jasonravagli/gym-manager/issues/5

--- logic-module/src/main/java/it/jasonravagli/gym/logic/Example.java
+++ logic-module/src/main/java/it/jasonravagli/gym/logic/Example.java
@@ -1,7 +1,12 @@
 package it.jasonravagli.gym.logic;
 
 public class Example {
-	public void example() {
-		System.out.println("This is an example");
+	public int example(boolean add) {
+		int value = 0;
+		if(add) {
+			value += 1;
+		}
+		
+		return value;
 	}
 }
